92e563456fe25838ab1a2f3a0b032ddda601b0f3
Varad Makarand Naniwadekar <159233068+varad1131@users.noreply.github.com>
2026-05-14 07:48:04 -0700

    Student_Grade_Calculator.java

---
+++ Student_Grade_Calculator.java
@@ -0,0 +1,47 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+class Number_Guessing_Game {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int minRange = 1;
+        int maxRange = 100;
+        int attemptsLimit = 10;
+        int score = 0;
+        System.out.println("Welcome to the Number Guessing Game!");
+
+        String playAgain;
+        do {
+            int generatedNumber = random.nextInt(maxRange - minRange + 1) + minRange;
+            System.out.println("\nGUESS THE NUMBER BETWEEN " + minRange + " AND " + maxRange + ":");
+
+            for(int attempts = 1; attempts <= attemptsLimit; ++attempts) {
+                System.out.print("ATTEMPT " + attempts + ": ");
+                int userGuess = scanner.nextInt();
+                if (userGuess == generatedNumber) {
+                    System.out.println("KUDOS ! YOU HAVE GUESSED THE ACCURATE NUMBER.");
+                    score += attempts;
+                    break;
+                }
+
+                if (userGuess < generatedNumber) {
+                    System.out.println("TOO LOW. GIVE ANOTHER TRY.");
+                } else {
+                    System.out.println("TOO HIGH. GIVE ANOTHER TRY.");
+                }
+
+                if (attempts == attemptsLimit) {
+                    System.out.println("SORRY, YOU HAVE ATTEMPTED MAXIMUM NUMBER OF TRIES. THE EXACT NUMBER WAS: " + generatedNumber);
+                }
+            }
+
+            System.out.print("DO YOU WANT TO TRY AGAIN ? (YES/NO): ");
+            playAgain = scanner.next().toLowerCase();
+        } while(!playAgain.equals("NO"));
+
+        System.out.println("YOU HAVE SCORED: " + score);
+        scanner.close();
+    }
+}
